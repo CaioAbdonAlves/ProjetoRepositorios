77a53cf9de5bab40c679bd5d70a7520be7a498fd
CaioAbdonA <caio.alves54@etec.sp.gov.br>
2023-01-29 16:08:37 -0300

    criação de estado e busca na api
    criação de estado para capturar texto do input e para guardar as buscas realizadas. Importação da api e função que dá um get passando como parâmetro o item digitado no input

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,34 @@
+import { useState, useCallback } from 'react';
 import { FaGithub, FaPlus } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles';
+import api from '../../services/api';
 
 export default function Main() {
+    const [newRepo, setNewRepo] = useState('');
+    const [repositorios, setRepositorios] = useState([]);
+
+    const handleSubmit = useCallback((e) => {
+        e.preventDefault();
+        async function submit() {
+
+            const response = await api.get(`repos/${newRepo}`);
+            
+            const data = {
+                name: response.data.full_name,
+    
+            }
+    
+            setRepositorios([...repositorios, data]);
+            setNewRepo('');
+        }
+
+        submit();
+    }, [newRepo, repositorios]);
+
+    function handleInputChange(e) {
+        setNewRepo(e.target.value);
+    }
+
     return(
         <div>
             <Container>
@@ -10,8 +37,13 @@ export default function Main() {
                     Meus Repositórios
                 </h1>
 
-                <Form onSubmit={() => {}}>
-                    <input type="text" placeholder='Adicionar Repositórios' />
+                <Form onSubmit={handleSubmit}>
+                    <input 
+                        type="text" 
+                        placeholder='Adicionar Repositórios' 
+                        value={newRepo}
+                        onChange={handleInputChange}
+                    />
                     
                     <SubmitButton>
                         <FaPlus color="#FFF" size={14}/>
