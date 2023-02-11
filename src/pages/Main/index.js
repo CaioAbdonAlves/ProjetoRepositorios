ca9c3d84c8c409b2a76c49154f5bb6b57ef473e6
CaioAbdonA <caio.alves54@etec.sp.gov.br>
2023-02-10 21:38:48 -0300

    Finalizando o App

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,47 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
 import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
 
 export default function Main() {
     const [newRepo, setNewRepo] = useState('');
     const [repositorios, setRepositorios] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [alert, setAlert] = useState(null);
+
+    // Buscar
+    useEffect(() => {
+        const repoStorage = localStorage.getItem('repos');
+        if(repoStorage) {
+            setRepositorios(JSON.parse(repoStorage));
+        }
+    }, [])
+
+    // Salvar Alterações
+    useEffect(() => {
+        localStorage.setItem('repos', JSON.stringify(repositorios));
+    }, [repositorios]);
+
 
     const handleSubmit = useCallback((e) => {
         e.preventDefault();
         async function submit() {
             setLoading(true);
+            setAlert(null);
             try {
+
+                if(newRepo === '') {
+                    throw new Error('Você precisa digitar o nome de algum repositório');
+                }
+
                 const response = await api.get(`repos/${newRepo}`);
+
+                const hasRepo = repositorios.find((repositorio) => repositorio.name === newRepo);
+
+                if(hasRepo) {
+                    throw new Error('Repositório duplicado');
+                }
                 
                 const data = {
                     name: response.data.full_name,
@@ -23,6 +51,7 @@ export default function Main() {
                 setRepositorios([...repositorios, data]);
                 setNewRepo('');
             } catch(error) {
+                setAlert(true);
                 console.log("Ocorreu um erro ao buscar o repositório " + error);
             } finally {
                 setLoading(false);
@@ -35,6 +64,7 @@ export default function Main() {
     }, [newRepo, repositorios]);
 
     function handleInputChange(e) {
+        setAlert(null);
         setNewRepo(e.target.value);
     }
 
@@ -51,7 +81,7 @@ export default function Main() {
                     Meus Repositórios
                 </h1>
 
-                <Form onSubmit={handleSubmit}>
+                <Form onSubmit={handleSubmit} error={alert}>
                     <input 
                         type="text" 
                         placeholder='Adicionar Repositórios' 
@@ -78,9 +108,9 @@ export default function Main() {
                                 </DeleteButton>
                                 {repositorio.name}
                             </span>
-                            <a href="">
+                            <Link to={`/repositorio/${encodeURIComponent(repositorio.name)}`}>
                                 <FaBars size={20}/>
-                            </a>
+                            </Link>
                         </li>
                     ))}
                 </List>
--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,8 +1,153 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Container } from "./styles";
+import api from "../../services/api";
+import { Owner, Loading, BackButton, IssuesList, PageActions, IssuesActions } from "./styles";
+import { FaArrowLeft } from 'react-icons/fa';
 
 export default function Repositorio() {
+    const [repo, setRepo] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [type, setType] = useState('open');
+    const { repositorio } = useParams();
+    const repositoryName = decodeURIComponent(repositorio);
+
+    useEffect(() => {
+        async function loadRepo() {
+
+           const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${repositoryName}`),
+                api.get(`/repos/${repositoryName}/issues`, {
+                    params: {
+                        state: type,
+                        per_page: 5
+                    }
+                })
+            ]);
+            setRepo(repositorioData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
+        }
+
+        loadRepo();
+    }, [repositorio]);
+
+    useEffect(() => {
+
+        async function loadIssue() {
+            const nomeRepo = decodeURIComponent(repositorio);
+
+            const response = await api.get(`/repos/${nomeRepo}/issues`, {
+                params: {
+                    state: type,
+                    page,
+                    per_page: '5', 
+                }
+            });
+
+            setIssues(response.data);
+            
+        }
+
+        loadIssue();
+
+    }, [page, type]);
+
+    useEffect(() => {
+        async function loadType() {
+            const nomeRepo = decodeURIComponent(repositorio);
+
+            const response = api.get(`/repos/${nomeRepo}/issues`, {
+                params: {
+                    state: type,
+                    page,
+                    per_page: 5,
+                }
+            });
+            
+            console.log(response)
+        }
+
+        loadType();
+
+    }, [type]);
+
+    function handlePage(action) {
+        setPage(action === 'back' ? page - 1 : page + 1)
+    }
+
+    function handleType(type) {
+        if(type === 'all'){
+            setType('all');
+        }
+
+        if(type === 'open') {
+            setType('open');
+        }
+
+        if(type === 'closed') {
+            setType('closed');
+        }
+    }
+
+    if(loading) {
+        return(
+            <Loading>
+                <h1>Carregando...</h1>
+            </Loading>
+        );
+    }
+
     return(
         <div>
-            <h1>Repositorio</h1>
+            <Container>
+                <BackButton to="/">
+                        <FaArrowLeft size={30} color="#000"/>
+                </BackButton>
+                <Owner>
+                    <img 
+                    src={repo.owner.avatar_url} 
+                    alt={repo.owner.login} 
+                    />
+                    <h1>{repo.name}</h1>
+                    <p>{repo.description}</p>
+                </Owner>
+
+                <IssuesActions>
+                    <button type="button" onClick={() => handleType('all')}>Todas</button>
+                    <button type="button" onClick={() => handleType('open')}>Abertas</button>
+                    <button type="button" onClick={() => handleType('closed')}>Fechadas</button>
+                </IssuesActions>
+
+                <IssuesList>
+                    {issues.map((issue) => (
+                        <li key={String(issue.id)}>
+                            <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+                            <div>
+                                <strong>
+                                    <a href={issue.html_url}>{issue.title}</a>
+
+                                    {issue.labels.map((label) => (
+                                        <span key={String(label.id)}>{label.name}</span>
+                                    ))}
+                                </strong>
+                                <p>{issue.user.login}</p>
+                            </div>
+
+                        </li>
+                    ))}
+                </IssuesList>
+
+                <PageActions>
+                    <button type="button" onClick={() => handlePage('back')} disabled={page < 2}>Voltar</button>
+                    <button type="button" onClick={() => handlePage('next')}>Próxima</button>
+                </PageActions>
+
+            </Container>
+            
         </div>
     );
 }
